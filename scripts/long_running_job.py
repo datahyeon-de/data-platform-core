272f360c5ee5992d01f38f9e5ac06d79423d3d15
datahyeon <hyeondata@gmail.com>
2026-01-04 16:05:57 +0900

    feat: Update spark ui proxy setting files #3

--- scripts/long_running_job.py
+++ scripts/long_running_job.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder.appName("Airflow-Spark-Long-Test").getOrCreate()
 
 print("Spark Job 시작! 3분간 대기하며 리소스를 점유합니다.")
 # 리소스를 점유하며 유지되는 시간을 벌기 위해 sleep 사용
-time.sleep(180) 
+time.sleep(360) 
 
 df = spark.createDataFrame([(1, "Success"), (2, "Running")], ["id", "status"])
 df.show()
